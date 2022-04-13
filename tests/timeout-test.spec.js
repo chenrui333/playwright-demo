889e77524e9f16f61793c84cc98bd2fdfc98ea41
Rui Chen <rui@chenrui.dev>
2022-04-13 16:01:50 -0400

    update ui test

--- tests/timeout-test.spec.js
+++ tests/timeout-test.spec.js
@@ -9,13 +9,12 @@ test.use({
 test('should work', async({page}) => {
   // Go to https://playwright.dev/
   await page.goto('https://playwright.dev/');
-
   // Click text=Search⌘K
   await page.locator('text=Search⌘K').click();
-
+  // Press CapsLock
+  await page.locator('[placeholder="Search docs"]').press('CapsLock');
   // Fill [placeholder="Search docs"]
-  await page.locator('[placeholder="Search docs"]').fill('command line tools');
-
+  await page.locator('[placeholder="Search docs"]').fill('Command line tools');
   // Click div[role="button"]:has-text("CancelGuidesCommand line toolsUsage​Command line toolsInstall browsers​Command l")
   await page.locator('div[role="button"]:has-text("CancelGuidesCommand line toolsUsage​Command line toolsInstall browsers​Command l")').click();
   await expect(page).toHaveURL('https://playwright.dev/docs/cli#generate-code');
